20256b99bcd6d3e9b82afa88a4c4df3e897301da
Gabeif7 <105760989+Gabeif7@users.noreply.github.com>
2023-06-04 10:08:48 -0400

    working on getting all images movable

--- index.js
+++ index.js
@@ -6,25 +6,81 @@ let direction = null;
 let x = 100;
 let y = 250;
 move(character).to(100, 250);
-/*function moveCharacter() {
-    if(direction === 'west') {
-         x = x - 1
+
+function move(element) {
+    element.style.position = 'fixed';
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px';
+        element.style.bottom = bottom + 'px';
     }
-    if(direction === 'north') {
-        y = y + 1
+
+    function moveWithArrowKeys(left, bottom) {
+        let directon = null;
+        let x = left;
+        let y = bottom;
+    
+        element.style.left = x + 'px';
+        element.style.bottom = y + 'px';
+
+        function moveCharacter() {
+            if(direction === 'west'){
+                x -= 1;
+            }
+            if(direction === 'north'){
+                y += 1;
+            }
+            if(direction === 'east'){
+                x += 1;
+            }
+            if(direction === 'south'){
+                y -= 1;
+            }
+            element.style.left = x + 'px';
+            element.style.bottom = y + 'px';
+         }
+
+    setInterval(moveCharacter, 1);
+
+    document.addEventListener('keydown', function(e){
+        if(e.repeat) return;
+        if(e.key === 'ArrowLeft'){
+            direction = 'west'
         }
-    if(direction === 'east') {
-        x = x + 1
+        if(e.key === 'ArrowUp'){
+            direction = 'north'
         }
-    if(direction === 'south') {
-        y = y - 1
+        if(e.key === 'ArrowRight'){
+            direction = 'east'
         }
-    character.style.left = x + 'px';
-    character.style.bottom = y + 'px';
+        if(e.key === 'ArrowDown'){
+            direction = 'south'
+        }
+        });
+
+    document.addEventListener('keyup', function(e){
+         direction = null;
+        });
     }
-    setInterval(moveCharacter, 1) ;
-*/
-setInterval(function(){ 
+
+    return {
+    to: moveToCoordinates,
+    withArrowKeys: moveWithArrowKeys
+    };
+}
+
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
+
+
+/*
+      setInterval(function(){ 
     if(direction === 'west'){
         x = x - 1
     }
@@ -41,12 +97,4 @@ setInterval(function(){
     character.style.bottom = y + 'px'
 }, 1)
 
-
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+*/
